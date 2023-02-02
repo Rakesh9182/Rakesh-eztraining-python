75dac31779630acdd8dd6a54384565d8444c53d6
Rakesh9182 <124118304+Rakesh9182@users.noreply.github.com>
2023-02-02 09:43:18 +0530

    Add files via upload

---
+++ DAY  2/day 2..py    1.py
@@ -0,0 +1,16 @@
+print('its','all','good','day')
+print('all','is','good')
+
+
+print('its','all','good','day',end='')
+print('all','is','good')
+
+
+print('its','all','good','day',end='*')
+print('all','is','good')
+
+
+print('its','all','good','day',end='')
+print('all','is','good',sep="***",end='')
+print("joy")
+  
---
+++ DAY  2/py.day 2....py
@@ -0,0 +1,32 @@
+
+
+ 
+t=58
+if t>60:
+ print("yes")
+else:
+ print("no")
+
+
+
+ t=60
+if t>55:
+ print("yes")
+elif t==60:
+    print("its equal")
+t=60
+if t>75:
+ print("yes")
+elif t==75:
+    print("its equal")
+else:
+    print("not>75 not equals also")
+
+
+t=60
+if t>75:
+ print("yes")
+elif t==75:
+    print("its equal")
+elif t>75:
+    print("its less")
